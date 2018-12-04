4f7422ef60534599546c261093e6a3dfb60f9776
Tony Mugendi <mugendi.tony26@gmail.com>
2018-12-04 12:42:13 +0300

    Added more Profile API Routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -30,6 +30,63 @@ router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
     }).catch(err => res.status(404).json(err))
 });
 
+// @route   GET api/profile/all
+// @desc    GET all Profiles
+// @access  Public
+router.get('/all', (req, res) => {
+    const errors = {}
+    Profile.find()
+        .populate('user', ['name', 'avatar'])
+        .then(profiles => {
+            if (!profiles) {
+                 errors.noprofile = 'There are no profiles'
+                 return res.status(404).json(errors)
+            }
+
+            res.json(profiles)
+        }).catch(err => res.status(404).json({profile: 'There are no profiles'}))
+})
+
+
+
+// @route   GET api/profile/handle/:handle
+// @desc    GET profile by users handle
+// @access  Public
+
+router.get('/handle/:handle', (req, res) => {
+    const errors = {}
+    Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+
+            res.json(profile)
+        }).catch(err => res.status(404).json(err))
+});
+
+
+// @route   GET api/profile/user/:user_id
+// @desc    GET profile by users ID
+// @access  Public
+
+router.get('/user/:user_id', (req, res) => {
+    const errors = {}
+    Profile.findOne({ user: req.params.user_id })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if (!profile) {
+                errors.noprofile = 'There is no profile for this user'
+                return res.status(404).json(errors)
+            }
+
+            res.json(profile)
+        }).catch(err => res.status(404).json({profile: 'There is no profile for this user'}))
+});
+
+
 
 // @route   POST api/profile/test
 // @desc    create or Update User's Profile
@@ -67,7 +124,7 @@ router.post('/', passport.authenticate('jwt', {session: false}), (req, res) => {
            Profile.findOne({ handle: profileFields.handle }).then(profile => {
                if (profile) {
                    errors.handle = 'The handle already exists'
-                   res.status(400).json(errors)
+                   return res.status(400).json(errors)
                }
 
                // Save profile
