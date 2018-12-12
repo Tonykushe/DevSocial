1afa30212264a867fbf38ee7a2dc3b6689624450
Tony Mugendi <mugendi.tony26@gmail.com>
2018-12-12 20:34:55 +0300

    Delete user account & profile API

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -187,7 +187,7 @@ router.post('/education', passport.authenticate('jwt', { session: false }), (req
 });
 
 
-// @route   POST api/profile/experience/:exp_id
+// @route   DELETE api/profile/experience/:exp_id
 // @desc    Delete experiencce from profile
 // @access  Private
 router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -206,7 +206,7 @@ router.delete('/experience/:exp_id', passport.authenticate('jwt', { session: fal
 
 });
 
-// @route   POST api/profile/education/:edu_id
+// @route   DELETE api/profile/education/:edu_id
 // @desc    Delete education from profile
 // @access  Private
 router.delete('/education/:edu_id', passport.authenticate('jwt', { session: false }), (req, res) => {
@@ -224,6 +224,19 @@ router.delete('/education/:edu_id', passport.authenticate('jwt', { session: fals
         }).catch(err => res.status(404).json(err))
 
 });
+
+// @route   DELETE api/profile
+// @desc    Delete user and profile
+// @access  Private
+router.delete('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+   Profile.findOneAndRemove({ user: req.user.id })
+    .then(() => {
+        User.findOneAndRemove({ _id: req.user.id })
+            .then(() => res.json({ success: true }))
+    })
+
+});
+
 
 
 
