83812d29497eda52336f4302cdd1064b14de01d2
Tony Mugendi <mugendi.tony26@gmail.com>
2018-12-25 20:45:11 +0300

    rename folder

---
+++ react/src/app/layout/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from '../../components/layout/Navbar';
+import Footer from '../../components/layout/Footer';
+import Landing from '../../components/layout/Landing';
+import LoginForm from '../../components/auth/Login/LoginForm'
+import RegisterForm from '../../components/auth/Register/RegisterForm'
+
+class App extends Component {
+  render() {
+    return (  
+        <Router>
+            <div className="App">
+                <Navbar />
+                <Route exact path='/' component={Landing}/>
+                <div className="container">
+                    <Route exact path='/register' component={RegisterForm} />
+                    <Route exact path='/login' component={LoginForm} />
+                
+                </div>
+                <Footer />
+            </div>
+        </Router>
+    );
+  }
+}
+
+export default App;
