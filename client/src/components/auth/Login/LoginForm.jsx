34089a9ff0d6833f42602b288fa1e173432323b0
Tony Mugendi <mugendi.tony26@gmail.com>
2019-01-06 22:03:04 +0300

    Create profile form

--- client/src/components/auth/Login/LoginForm.jsx
+++ client/src/components/auth/Login/LoginForm.jsx
@@ -65,7 +65,7 @@ class LoginForm extends Component {
             <div className="login">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-8 m-auto">
+                        <div className="col-md-5 m-auto">
                             <h1 className="display-4 text-center">Log In</h1>
                             <p className="lead text-center">Sign in to your DevSocial account</p>
                             <form onSubmit={this.onSubmit}>
