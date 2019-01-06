f60b35129ead1c7a75389da6c2af6c4d45fb9653
Tony Mugendi <mugendi.tony26@gmail.com>
2019-01-06 14:03:05 +0300

    Private Route component

--- client/src/app/layout/App.js
+++ client/src/app/layout/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from '../common/PrivateRoute';
 import Navbar from '../../components/layout/Navbar';
 import Footer from '../../components/layout/Footer';
 import Landing from '../../components/layout/Landing';
@@ -18,8 +19,9 @@ class App extends Component {
                 <div className="container">
                     <Route exact path='/register' component={RegisterForm} />
                     <Route exact path='/login' component={LoginForm} />
-                    <Route exact path='/dashboard' component={Dashboard} />
-                
+                    <Switch>
+                        <PrivateRoute exact path='/dashboard' component={Dashboard} />
+                    </Switch>
                 </div>
                 <Footer />
             </div>
